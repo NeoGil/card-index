c65eb67b58fb4df3e4bf14cfce67ec12b862437d
Denis Solovev <neogil28@gmail.com>
2023-06-16 14:22:48 +0300

    Added the Admin LTE template, as well as the Login view.

---
+++ resources/lang/en/public.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    "login_title" => "Sign in to start your session",
+    "email" => "E-Mail Address",
+    "password" => "Password",
+    "sing_in" => "Sing In"
+];
--- resources/views/Pub/Auth/login.blade.php
+++ resources/views/Pub/Auth/login.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 
 
-    <div class="content d-flex justify-content-center align-items-center">
+    {{--<div class="content d-flex justify-content-center align-items-center">
 
         <!-- Login form -->
         <form method="POST" class="login-form" action="{{ route('login.post') }}">
@@ -46,6 +46,42 @@
         </form>
         <!-- /login form -->
 
-    </div>
+    </div>--}}
+    <div class="card">
+        <div class="card-body login-card-body">
+            <p class="login-box-msg">{{__('public.login_title')}}</p>
 
+            <form method="POST" class="login-form" action="{{ route('login') }}">
+                <div class="input-group mb-3">
+                    <input id="email" type="email"
+                           class="form-control {{ $errors->has('email') ? ' is-invalid' : '' }}"
+                            placeholder="{{ __('public.email') }}" name="email" value="{{ old('email') }}" required autofocus>
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-envelope"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="input-group mb-3">
+                    <input id="password" type="password"
+                           class="form-control {{ $errors->has('password') ? ' is-invalid' : '' }}"
+                           name="password" placeholder="{{ __('public.password') }}" required>
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-lock"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-8">
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-4">
+                        <button type="submit" class="btn btn-primary btn-block">{{ __('public.sing_in') }}</button>
+                    </div>
+                    <!-- /.col -->
+                </div>
+            </form>
+        <!-- /.login-card-body -->
+    </div>
 @endsection
